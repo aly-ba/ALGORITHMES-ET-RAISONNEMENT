04ad98ff7d756251b0d567dcf8db25ea699874e5
aly ba <alybas2002@yahoo.fr>
2014-06-04 11:28:41 +0200

    submit r&d mises à  jour

---
+++ Networking/queryDatabase/02_02.sql
@@ -0,0 +1,16 @@
+USE AdventureWorks2012;
+GO
+
+-- Write a SELECT statement for the Employee table
+-- selecting specific attributes and using an alias for the table
+SELECT hre.LoginID, hre.JobTitle, hre.HireDate
+FROM HumanResources.Employee AS hre;
+GO
+
+
+-- Write a SELECT statement for the Employee table
+-- selecting specific attributes and using an alias for the table
+-- but include aliases for the attribute column names as well
+SELECT hre.LoginID AS [login], hre.JobTitle AS [Title],hre.HireDate AS [Date Hired]
+FROM HumanResources.Employee AS hre;
+GO
---
+++ Networking/queryDatabase/02_04.sql
@@ -0,0 +1,26 @@
+USE AdventureWorks2012;
+GO
+
+-- SELECT first and last name and person type
+-- from the person table to show the cryptic
+-- values used to represent the person type
+SELECT FirstName, LastName, PersonType 
+FROM Person.Person;
+GO
+
+
+-- implement the CASE statement to convert the
+-- cryptic values store in the DB, to more user
+-- friendly values in the output
+SELECT FirstName, LastName,
+CASE PersonType
+		WHEN 'SC' THEN 'Store Contact'
+		WHEN 'IN' THEN 'Individual Customer'
+		WHEN 'SP' THEN 'Sales Person'
+		WHEN 'EM' THEN 'Employee'
+		WHEN 'VC' THEN 'Vendor Contact'
+		WHEN 'GC' THEN 'General Contact'
+		ELSE 'Unknown Person Type'
+END AS [Type of Contact]
+FROM Person.Person;
+GO
